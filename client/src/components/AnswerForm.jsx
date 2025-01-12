8190a20b93c73a29f94f5521d51c9c2e369c6cb8
idanye <idanyehiel@gmail.com>
2025-01-12 10:34:45 +0200

    Added a page class to the AnswerForm for uniformity

--- client/src/components/AnswerForm.jsx
+++ client/src/components/AnswerForm.jsx
@@ -16,37 +16,39 @@ const AnswerForm = () => {
   };
 
   return (
-    <div className="answer-page-container">
-      <div className="answer-form-selector">
-        <button className="go-back-button" onClick={handleBack}>Back</button>
-        <div className="role-selection">
-          <p>Select Your Role:</p>
-          <label>
-            <input
-              type="radio"
-              name="userRole"
-              value="expert"
-              checked={userRole === "expert"}
-              onChange={handleRoleChange}
-            />
-            Expert
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="userRole"
-              value="regular user"
-              checked={userRole === "regular user"}
-              onChange={handleRoleChange}
-            />
-            Regular user
-          </label>
-        </div>
+      <div className="page">
+        <div className="answer-page-container">
+          <div className="answer-form-selector">
+            <button className="go-back-button" onClick={handleBack}>Back</button>
+            <div className="role-selection">
+              <p>Select Your Role:</p>
+              <label>
+                <input
+                  type="radio"
+                  name="userRole"
+                  value="expert"
+                  checked={userRole === "expert"}
+                  onChange={handleRoleChange}
+                />
+                Expert
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="userRole"
+                  value="regular user"
+                  checked={userRole === "regular user"}
+                  onChange={handleRoleChange}
+                />
+                Regular user
+              </label>
+            </div>
 
-        {userRole === "expert" && <AnswerFormExpert />}  
-        {userRole === "regular user" && <AnswerFormRegularUser />} 
+            {userRole === "expert" && <AnswerFormExpert />}
+            {userRole === "regular user" && <AnswerFormRegularUser />}
+          </div>
+        </div>
       </div>
-    </div>
   );
 };
 
